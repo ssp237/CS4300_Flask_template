169eb3a153432163780374cf3cf5791ad09e63b8
Shan Parikh <ssp237@cornell.edu>
2021-04-19 01:17:52 -0400

    preliminary search, incorrect results

--- app/irsystem/controllers/__init__.py
+++ app/irsystem/controllers/__init__.py
@@ -1,15 +1,15 @@
 # Import flask deps
 from flask import request, render_template, \
-	flash, g, session, redirect, url_for, jsonify, abort
+    flash, g, session, redirect, url_for, jsonify, abort
 
 # For decorators around routes
-from functools import wraps 
+from functools import wraps
 
 # Import for pass / encryption 
-from werkzeug import check_password_hash, generate_password_hash 
+from werkzeug import check_password_hash, generate_password_hash
 
 # Import the db object from main app module
-from app import db 
+from app import db
 
 # Marshmallow 
 from marshmallow import ValidationError
@@ -21,8 +21,66 @@ from app import socketio
 # from app.irsystem import search
 
 # IMPORT THE BLUEPRINT APP OBJECT 
-from app.irsystem import irsystem 
+from app.irsystem import irsystem
 
 # Import module models
 from app.accounts.models.user import *
 from app.accounts.models.session import *
+
+# Import json (TODO should this be loaded here???)
+# TODO: wrap in helper function
+import json
+
+
+def getProducts(i_list, p_data):
+    """
+    Return a list of the products that contain at least one ingredient
+    in the given list.
+    :param i_list: List of ingredients to search for
+    :param p_data: Product data to search in
+    :return: List of products that contain at least one ingredient in i_list
+    """
+    i_set = set(i_list)
+    out = []
+    for k, v in p_data.items():
+        i = 0
+        found = False
+        while i < len(v['ingredients']) and not found:
+            found = v['ingredients'][i] in i_set
+            i += 1
+        if found:
+            out.append(k)
+
+    return out
+
+
+with open("finaldata.json", "r") as f:
+    data = json.loads("\n".join(f.readlines()))
+
+with open("ingredients.json", "r") as f:
+    u_ingredients = json.loads("\n".join(f.readlines()))
+
+with open("concerns.json", "r") as f:
+    u_concerns = json.loads("\n".join(f.readlines()))
+
+# Mild preprocessing (should eventually move to data scraping step)
+for _, v in data.items():
+    v['ingredients'] = v['ingredients'].split(",")
+
+categories = {}
+# TODO: For next time: change products to a counter dictionary! then weigh by counts
+# so {'B': 4} means 4 of B's ingredients are antioxidants
+
+for k, v in u_concerns.items():
+    d = {
+        "concerns": ", ".join(v),
+        "products": getProducts(u_ingredients[k], data)
+    }
+    categories[k.lower()] = d
+
+# Construct auxiliary data structures
+num_products = len(data)
+category_to_index = {name:index for index, name in enumerate(categories)}
+products_to_indices = {k: v for k, v in zip(data.keys(), range(num_products))}
+indices_to_products = {v: k for k, v in products_to_indices.items()}
+
--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,20 +1,84 @@
-from . import *  
+from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 project_name = "Pocket Esthetician"
 net_id = "Em Gregoire: erg92 \nKriti Sinha: ks867 \nRaheel Yanful: ray37 \nShan Parikh: ssp237"
 
+
+def cos_sim(c, tfidf_mat, category_to_idx):
+    """Returns the cosine similarity of the query and a concern list.
+
+    Params: {c: String,
+             tfidf_mat: np.ndarray,
+             concern_to_index: Dict}
+    Returns: Float
+    """
+    # query is last row
+    v1 = tfidf_mat[len(tfidf_mat) - 1]
+    v2 = tfidf_mat[category_to_idx[c]]
+    num = np.dot(v1, v2)
+
+    return num / (np.linalg.norm(v1) * np.linalg.norm(v2))
+
+
+def concern_similarity(query, category_info, prod_to_idx, category_to_idx):
+    """ Finds cosine similarity between input query (concerns) and each product category's concern list.
+        Returns a numpy array with each product's score, based on the categories they are in.
+
+        Params: {query: (user input) String,
+                 category_info: (category -> Dict) Dict,
+                 prod_to_idx: (product -> index) Dict,
+                 category_to_idx: (category -> index) Dict}
+        Returns: Numpy Array
+    """
+    result = np.zeros(len(prod_to_idx))
+
+    tfidf_vec = TfidfVectorizer(stop_words='english')
+    lst = [category_info[k]['concerns'] for k in categories.keys()]
+    lst.append(query)
+    tfidf_mat = tfidf_vec.fit_transform(lst).toarray()
+
+    for k, v in category_info.items():
+        sim = cos_sim(k, tfidf_mat, category_to_idx)
+        for p in v['products']:
+            result[prod_to_idx[p]] += sim
+
+    # for invalid query
+    if sum(result) == 0: return 'invalid query'
+    return result
+
+
+def rank_products(query, category_info, prod_to_idx, idx_to_prod, product_info, category_to_idx):
+    """ Returns a ranked list of products, with the most relevant at index 0.
+
+        Params: {query: (user input) String,
+                 category_info: (category -> Dict) Dict,
+                 prod_to_idx: (product -> index) Dict,
+                 idx_to_prod: (index -> product) Dict
+                 product_info: (product -> Dict) Dict
+        Returns: List
+    """
+    scores = concern_similarity(query, category_info, prod_to_idx, category_to_idx)
+
+    scores_idx = [(val, prod) for prod, val in enumerate(scores)]
+    rank_idx = sorted(scores_idx, key=lambda x: (x[0], product_info[idx_to_prod[x[1]]]["num faves"],
+                                                 product_info[idx_to_prod[x[1]]]["price"]), reverse=True)
+    ranking = list(map(lambda x: idx_to_prod[x[1]], rank_idx))
+    return ranking
+
+
 @irsystem.route('/', methods=['GET'])
 def search():
-	query = request.args.get('search')
-	if not query:
-		data = []
-		output_message = ''
-	else:
-		output_message = "Your search: " + query
-		data = range(5)
-	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
+    query = request.args.get('search')
+    if not query:
+        search_data = []
+        output_message = ''
+    else:
+        output_message = "Your search: " + query
+        search_data = rank_products(query, categories, products_to_indices,
+                                    indices_to_products, data, category_to_index)
 
-
-
+    return render_template('search.html', name=project_name, netid=net_id,
+                           output_message=output_message, data=search_data[:10])
