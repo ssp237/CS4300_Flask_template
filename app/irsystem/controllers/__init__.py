b9c3fc0e4b3f40f0d74c8a18f7d5e31b2e5d456e
Kriti Sinha <ks867@cornell.edu>
2021-04-30 22:32:10 -0400

    new ranking added

--- app/irsystem/controllers/__init__.py
+++ app/irsystem/controllers/__init__.py
@@ -29,7 +29,10 @@ from app.accounts.models.session import *
 
 # Import json (TODO should this be loaded here???)
 # TODO: wrap in helper function
+import numpy as np
 import json
+import glob
+import re
 
 
 def getProducts(i_list, p_data):
@@ -57,19 +60,24 @@ def getProducts(i_list, p_data):
 with open("finaldata.json", "r") as f:
     data = json.loads("\n".join(f.readlines()))
 
-with open("ingredients.json", "r") as f:
+with open("ingredients.json", "r", errors='ignore') as f:
     u_ingredients = json.loads("\n".join(f.readlines()))
 
 with open("concerns.json", "r") as f:
     u_concerns = json.loads("\n".join(f.readlines()))
+    
+with open("relevant_types.json", "r") as f:
+    relevant_product_types = json.loads("\n".join(f.readlines()))
+    
 
-# Mild preprocessing (should eventually move to data scraping step)
+# Cleaning ingredients
 for _, v in data.items():
-    v['ingredients'] = v['ingredients'].split(",")
+    i_string = v['ingredients']
+    if i_string[-1] == '.': i_string = i_string[:len(i_string) - 1]
+    i_list = re.split(", |\. ", i_string)
+    v['ingredients'] = [(re.sub(r".*:", "", s)).strip() for s in i_list]
 
 categories = {}
-# TODO: For next time: change products to a counter dictionary! then weigh by counts
-# so {'B': 4} means 4 of B's ingredients are antioxidants
 
 for k, v in u_concerns.items():
     d = {
@@ -83,4 +91,56 @@ num_products = len(data)
 category_to_index = {name: index for index, name in enumerate(categories)}
 products_to_indices = {k: v for k, v in zip(data.keys(), range(num_products))}
 indices_to_products = {v: k for k, v in products_to_indices.items()}
+
+def create_product_types_dict():
+    """Returns a dictionary to map product types to Boolean arrays 
+    (True if index corresponds to a product of that type).
+    
+    Params: {}
+    Returns: (String -> Numpy Array) Dict 
+    """
+    product_types = {}
+    product_files = glob.glob("./product_types_lists/*.json")
+    if len(product_files) == 0: 
+        print('can\'t read')  
+    
+    for p_file in product_files:
+        name = p_file[22:-5]
+        with open(p_file) as json_file:
+            data = json.load(json_file)
+        p_arr = np.full(num_products, False)
+        for p in data:
+            if p not in products_to_indices.keys():
+                continue
+            p_arr[products_to_indices[p]] = True
+        product_types[name] = p_arr
+    
+    return product_types
+
+def create_price_ranges():
+    """Returns a dictionary to map price ranges to Boolean arrays 
+    (True if index corresponds to a product within the budget).
+    
+    Params: {}
+    Returns: (String -> Numpy Array) Dict 
+    """
+    price_ranges = dict([('under $15', np.full(num_products, False)), ('$15-30', np.full(num_products, False)),
+                         ('$30-50', np.full(num_products, False)), ('$50-75', np.full(num_products, False)), 
+                         ('$75+', np.full(num_products, False))])
+    for k,v in data.items():
+        if v['price'] < 15:
+            price_ranges['under $15'][products_to_indices[k]] = True
+        elif v['price'] < 30:
+            price_ranges['$15-30'][products_to_indices[k]] = True
+        elif v['price'] < 30:
+            price_ranges['$30-50'][products_to_indices[k]] = True
+        elif v['price'] < 30:
+            price_ranges['$50-75'][products_to_indices[k]] = True
+        else :
+            price_ranges['$75+'][products_to_indices[k]] = True
+    
+    return price_ranges
+
+product_types = create_product_types_dict()
+price_ranges = create_price_ranges()
 
